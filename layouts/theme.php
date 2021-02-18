6b02e28845d6f9b64794aae5713c6df79692f0ef
Christopher Jones <tkcwebdev@gmail.com>
2021-02-17 22:15:57 -0800

    Updated to everything thats on prod

--- layouts/theme.php
+++ layouts/theme.php
@@ -16,9 +16,20 @@ include($this['path']->path('layouts:theme.config.php'));
 
 <head>
 	<?php echo $this['template']->render('head'); ?>
+	<!-- Lumn8 Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N9S6XJV');</script>
+<!-- End Lumn8 Google Tag Manager -->
 </head>
 
 <body class="<?php echo $this['config']->get('body_classes'); ?>">
+	<!-- Lumn8 Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9S6XJV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Lumn8 Google Tag Manager (noscript) -->
 
 	<?php if ($this['widgets']->count('toolbar-l + toolbar-r')) : ?>
 		<div class="tm-toolbar uk-clearfix uk-hidden-small">
@@ -35,7 +46,7 @@ include($this['path']->path('layouts:theme.config.php'));
 	<?php endif; ?>
 
 	<?php if ($this['widgets']->count('logo + headerbar')) : ?>
-		<div class="tm-headerbar uk-clearfix uk-hidden-small">
+		<div class="tm-headerbar uk-clearfix">
 
 
 			<div class="nav-container">
@@ -47,6 +58,8 @@ include($this['path']->path('layouts:theme.config.php'));
 
 						<?php if ($this['widgets']->count('menu + search')) : ?>
 							<nav class="tm-navbar uk-navbar">
+
+								<a href="#connect" class="btn primary nav-cta">Let's Talk Growth</a>
 
 								<?php if ($this['widgets']->count('offcanvas')) : ?>
 									<a href="#offcanvas" data-uk-offcanvas="{mode:'slide'}"><img class="toggler" src="/wp-content/themes/yoo_master2_wp/images/navmenu_icon.svg" alt="Menu Icon" /></a>
@@ -158,8 +171,9 @@ include($this['path']->path('layouts:theme.config.php'));
 	<?php endif; ?>
 	
 </body>
-<video autoplay muted loop id="bgvideo">
+<video autoplay muted loop playsinline id="bgvideo">
 		<source src="/wp-content/uploads/2020/12/LUMN8-Splash-Page-Background-1.mp4" type="video/mp4"/>
+		<source src="/wp-content/uploads/2020/12/LUMN8-Splash-Page-Background-1.webm" type="video/webm"/>
 		Your browser does not support HTML5 video.
 </video>
 </html>
